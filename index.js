5d278d8c71283da1a781a6ea7ee2790aa564dcb3
hiteshagja <hiteshagja24@gmail.com>
2019-10-30 15:38:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,43 @@
-import React from "react";
-import { Modal, StyleSheet, TouchableOpacity, View } from "react-native";
-import Button from "./button";
-import FadeInView from "./fade_in_view";
+import React from 'react';
+import {Modal, StyleSheet, TouchableOpacity, View} from 'react-native';
+import Button from './button';
+import FadeInView from './fade_in_view';
 
-const ActionModal = ({ modalVisible, backgroundColor, buttonText, children, onCancel, cancelButton }) => (
-  <FadeInView visible={modalVisible} backgroundColor={backgroundColor}>
-    <Modal
-      animationType="slide"
-      transparent={true}
-      visible={modalVisible}
-      onRequestClose={onCancel}>
-      <View style={styles.modalContainer}>
-        <TouchableOpacity style={styles.container} onPress={onCancel}></TouchableOpacity>
-        {children}
-        {cancelButton && <Button onPress={onCancel} text={buttonText || "Cancel"} />}
-      </View>
-    </Modal>
-  </FadeInView>
+const ActionModal = ({
+  modalVisible,
+  backgroundColor,
+  buttonText,
+  children,
+  onCancel,
+  cancelButton,
+}) => (
+  <Modal
+    animationType="fade"
+    transparent={true}
+    visible={modalVisible}
+    onRequestClose={onCancel}>
+    <View style={styles.modalContainer}>
+      <TouchableOpacity
+        style={styles.container}
+        onPress={onCancel}></TouchableOpacity>
+      {children}
+      {cancelButton && (
+        <Button onPress={onCancel} text={buttonText || 'Cancel'} />
+      )}
+    </View>
+  </Modal>
 );
 
 var styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
   },
   modalContainer: {
     flex: 1,
-    justifyContent: "flex-end"
-  }
+    justifyContent: 'flex-end',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    paddingHorizontal: 10,
+  },
 });
 
 export default ActionModal;
