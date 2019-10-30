8b07a5e0410e8c5cc65e98ec3bc82bf8b3bd71f9
hiteshagja <hiteshagja24@gmail.com>
2019-10-30 15:56:50 +0530

    Update button.js

--- button.js
+++ button.js
@@ -1,11 +1,9 @@
-import React from "react";
-import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import React from 'react';
+import {StyleSheet, Text, TouchableOpacity} from 'react-native';
 
-const Button = ({ text, onPress }) => (
-  <TouchableOpacity style={styles.button} onPress={onPress}>
-    <Text style={styles.buttonText}>
-      {text}
-    </Text>
+const Button = ({text, onPress, color}) => (
+  <TouchableOpacity activeOpacity={1} style={styles.button} onPress={onPress}>
+    <Text style={[styles.buttonText, {color: color || '#0069d5'}]}>{text}</Text>
   </TouchableOpacity>
 );
 
@@ -13,18 +11,18 @@ var styles = StyleSheet.create({
   buttonText: {
     color: '#0069d5',
     alignSelf: 'center',
-    fontSize: 18
+    fontSize: 18,
   },
   button: {
-    height: 36,
+    height: 50,
     backgroundColor: 'white',
     borderColor: 'white',
     borderWidth: 1,
     borderRadius: 6,
     marginBottom: 10,
     alignSelf: 'stretch',
-    justifyContent: 'center'
-  }
+    justifyContent: 'center',
+  },
 });
 
 export default Button;
